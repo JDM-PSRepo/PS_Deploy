ea3ce71f10269ef03c374b56fd49338a14d84afb
JDM-PSRepo <JamesDMester@Gmail.com>
2025-02-05 13:35:08 -0500

    Removed Old Files not needed
    Just a bit of cleaning up

--- DeployFiles/LegalBypass/SoloWingRegistry.bat
+++
@@ -1,4 +0,0 @@
-::Nukes the registry values that create the clickthrough enviornment
-::If you dont get the joke go play Ace Combat Zero: Belkan War
-reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v LegalNoticeCaption /f
-reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v LegalNoticeText /f
